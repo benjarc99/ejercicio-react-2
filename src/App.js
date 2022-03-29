6aceb713b06343e6c3741a92e7622ca84fd4ae70
benji_rc_32@hotmail.com <benjarc99@gmail.com>
2022-03-29 17:14:20 -0300

    insercion de peticion fetch y variables de estado en App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Card from "./components/Card";
+import "./components/Cards.css";
+import Loader from "./components/Loader";
 
 function App() {
+  const [dataBase, setDataBase] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  let url = "https://reqres.in/api/users?per_page=12";
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url, { timeout: 3000 })
+      .then((res) => {
+        let data = res.data;
+
+        setDataBase(data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        let msgError = {
+          status: err.response.status || "0",
+          statusText: err.response.statusText || "Ocurrio un error",
+        };
+        setError(msgError);
+      });
+  }, [url]);
+
   return (
     <div className="main-cards">
-      <Card />
+      {loading && <Loader />}
+      <section className="container-cards">
+        {dataBase && dataBase.map((el, i) => <Card el={el} i={i} />)}
+      </section>
+      {error && (
+        <div className="msg-error">{`Error ${error.status}: ${error.statusText}`}</div>
+      )}
     </div>
   );
 }
