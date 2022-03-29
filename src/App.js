25aae59ebd7de86a069665b7801cb4e7e2ef1c7a
benji_rc_32@hotmail.com <benjarc99@gmail.com>
2022-03-29 19:35:18 -0300

    cambios en props de componente card

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./components/Cards.css";
 import Loader from "./components/Loader";
 
 function App() {
-  const [dataBase, setDataBase] = useState(null);
+  const [users, setUsers] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -18,8 +18,9 @@ function App() {
       .then((res) => {
         let data = res.data;
 
-        setDataBase(data.data);
+        setUsers(data.data);
         setLoading(false);
+        console.log(data.data);
       })
       .catch((err) => {
         let msgError = {
@@ -34,7 +35,15 @@ function App() {
     <div className="main-cards">
       {loading && <Loader />}
       <section className="container-cards">
-        {dataBase && dataBase.map((el, i) => <Card el={el} i={i} />)}
+        {users &&
+          users.map((user, i) => (
+            <Card
+              name={`${user.first_name} ${user.last_name}`}
+              img={user.avatar}
+              email={user.email}
+              key={i}
+            />
+          ))}
       </section>
       {error && (
         <div className="msg-error">{`Error ${error.status}: ${error.statusText}`}</div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,12 @@ import React from "react";
 import Photo from "./Photo";
 import Button from "./Button";
 
-const Card = ({ el, i }) => {
+const Card = ({ name, img, email }) => {
   return (
-    <div key={i} className="card">
-      <Photo src={el.avatar} />
-      <h4>{`${el.first_name} ${el.last_name}`}</h4>
-      <div className="text-card">{el.email}</div>
+    <div className="card">
+      <Photo src={img} />
+      <h4>{`${name}`}</h4>
+      <div className="text-card">{email}</div>
       <Button />
     </div>
   );
